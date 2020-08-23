fc23db24d197186a5a6ab05bd2aeb68a4be321b2
Mauricio Luis de Lorenzi <luis_mauricio_13_@hotmail.com>
2020-08-23 18:15:38 -0300

    added some actions for database, they are not done yet.

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
-const result = require('dotenv').config(),
+const { connect } = require('mongoose');
+
+const { connect, disconnect } = require('./services/database.js'),
+result = require('dotenv').config(),
 express = require('express')();
 
 if (result.error) {
   throw result.error
 }
 
+connect();
+disconnect();
 express.listen(process.env.APP_PORT);
